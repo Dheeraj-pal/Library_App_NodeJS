8da3620de1aed8296adc570d5eca6d6734704a30
Dheeraj-pal <dheeraj.pal19@gmail.com>
2023-07-07 14:34:02 +0530

    logger with more precise message

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,44 @@
-import { Request, Response, NextFunction } from 'express';
-import winston from 'winston';
+// import { Request, Response, NextFunction } from 'express';
+// import winston from 'winston';
+
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({ filename: 'logs.log' }),
+//   ],
+// });
+
+// export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//   logger.info(`${req.method} ${req.path}`);
+//   next();
+// };
+
+// export { logger };
+
+import { Request, Response, NextFunction } from "express";
+import winston from "winston";
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs.log' }),
+    new winston.transports.File({ filename: "logs.log" }),
   ],
 });
 
-export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  logger.info(`${req.method} ${req.path}`);
+export const loggerMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { method, path, ip } = req;
+  logger.info(`IP - ${ip}, METHOD - ${method}, PATH - ${path}`);
   next();
 };
 
