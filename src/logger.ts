ef0092dc4560a07431e04c95b1f1afe3d7d2f8cd
Dheeraj-pal <dheeraj.pal19@gmail.com>
2023-07-07 14:36:23 +0530

    updated logger with more precise message

--- src/logger.ts
+++ src/logger.ts
@@ -7,6 +7,7 @@ const logger = winston.createLogger({
     winston.format.timestamp(),
     winston.format.json()
   ),
+  
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({ filename: "logs.log" }),
